a11a16a448003343d7c76c559eb14034cb80ccfb
eduardotorresferreira <etf1974@hotmail.com>
2022-09-06 11:39:26 -0400

    Added Instrument Spinner

---
+++ app/src/main/java/com/example/myapplication/ui/Notes/InstrumentSpinner.kt
@@ -0,0 +1,4 @@
+package com.example.myapplication.ui.Notes
+
+class InstrumentSpinner {
+}
--- app/src/main/java/com/example/myapplication/ui/Notes/NotesFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/Notes/NotesFragment.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
+import com.example.myapplication.R
 import com.example.myapplication.databinding.FragmentNotesBinding
 
-class NotesFragment : Fragment() {
+class NotesFragment : Fragment() , AdapterView.OnItemSelectedListener{
 
     private lateinit var binding: FragmentNotesBinding
     //private val viewModel : NotesViewModel = ViewModelProvider(this)[NotesViewModel::class.java]
@@ -23,5 +26,23 @@ class NotesFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val spinnerAdapter = ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.instruments_array,
+            android.R.layout.simple_spinner_item)
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.spinnerInstrument.apply {
+            adapter = spinnerAdapter
+            onItemSelectedListener = this@NotesFragment
+        }
     }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, pos: Int, id: Long) {
+        // TODO submit sounds to adapter
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        // TODO clear adapter
+    }
+
 }
