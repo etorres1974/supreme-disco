4daeb6cdf7d25ca38cd454a4d7a98d77bc606e1e
eduardotorresferreira <etf1974@hotmail.com>
2022-09-06 11:17:49 -0400

    Refactor Notifications to Notes

---
+++ app/src/main/java/com/example/myapplication/ui/Notes/NotesFragment.kt
@@ -0,0 +1,27 @@
+package com.example.myapplication.ui.Notes
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.myapplication.databinding.FragmentNotesBinding
+
+class NotesFragment : Fragment() {
+
+    private lateinit var binding: FragmentNotesBinding
+    //private val viewModel : NotesViewModel = ViewModelProvider(this)[NotesViewModel::class.java]
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentNotesBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/ui/Notes/NotesViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.myapplication.ui.Notes
+
+import androidx.lifecycle.ViewModel
+
+class NotesViewModel : ViewModel() {
+}
